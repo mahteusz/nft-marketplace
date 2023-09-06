771193f44b3b64ce4416b7b5c04328d351079a80
Mateus Marques <mateusmarques200@gmail.com>
2023-09-05 23:34:30 -0300

    CHORE: cleaning code to build

--- src/components/UploadForm/types.ts
+++ src/components/UploadForm/types.ts
@@ -1,4 +1,4 @@
-import { useState, Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction } from 'react'
 
 type NFTAttributeProps = {
   outerSetAttributes: Dispatch<SetStateAction<string[]>>,
